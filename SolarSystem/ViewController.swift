c3e9ae37d04e3639a493aa702f120dfd051ee41e
Andre Campos <andretavera@gmail.com>
2018-02-22 09:15:10 -0300

    Initial Commit

---
+++ SolarSystem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SolarSystem
+//
+//  Created by andre campos on 22/02/18.
+//  Copyright © 2018 andre campos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
